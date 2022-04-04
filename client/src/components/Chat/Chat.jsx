3ecd422d5a59cc6e2f7042231e2640deecca6c4a
Gabriel Roberto <gabrielrobertoal@gmail.com>
2022-04-04 08:55:00 -0300

    recebimento das mensagens

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -9,6 +9,8 @@ let socket;
 const Chat = ({ location }) => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
 
   const ENDPOINT = "localhost:5000";
 
@@ -20,20 +22,46 @@ const Chat = ({ location }) => {
     setName(name);
     setRoom(room);
 
-    socket.emit('join', { name: name, room: room }, () => {
-
-    });
+    socket.emit("join", { name: name, room: room }, () => {});
 
     return () => {
-      socket.emit('disconnect');
+      socket.emit("disconnect");
 
       socket.off();
-    }
+    };
   }, [ENDPOINT, location.search]);
 
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages([...messages, message]);
+    });
+  }, [messages]);
+
+  // envio de mensagens
+  function sendMessage(event) {
+    event.preventDefault();
+
+    if (message) {
+      socket.emit("sendMessage", message, () => {
+        setMessage("");
+      });
+    }
+  }
+
+  console.log(message, messages)
+
   return (
-    <div className="main">
-      <h1>Main</h1>
+    <div className="outsideContainer">
+      <div className="container">
+        <input
+          type="text"
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+          onKeyPress={(event) =>
+            event.key === "Enter" ? sendMessage(event) : null
+          }
+        />
+      </div>
     </div>
   );
 };
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
+const cors = require('cors');
 
 const router = require("./router");
 const { addUser, getUser, removeUser, getUsersInRoom } = require("./users");
@@ -10,6 +11,9 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 // usado para requests em tempo real
+
+app.use(cors());
+app.use(router());
 
 // socketio config
 io.on("connection", (socket) => {
@@ -21,11 +25,25 @@ io.on("connection", (socket) => {
     }
 
     // mensagem ao usuário
-    socket.emit('message', { user: 'admin', text: `Bem vindo(a) ${user.name}, você está na sala ${user.room}`})
+    socket.emit("message", {
+      user: "admin",
+      text: `Bem vindo(a) ${user.name}, você está na sala ${user.room}`,
+    });
     // mensagem aos usuários da sala
-    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} entrou na sala.`})
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name} entrou na sala.` });
 
     socket.join(user.room);
+
+    callback();
+  });
+
+  // aguardando o evento de envio no backend
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit("message", { user: user.name, text: message });
 
     callback();
   });
